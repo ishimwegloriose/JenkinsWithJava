bd3fd2dff753d172797701b02de9f2fdfc74fd92
Ishimwe Gloriose <shimwegloria@gmail.com>
2021-02-17 11:57:34 +0200

    This is Hello world program

---
+++ Jenkins.java
@@ -0,0 +1,14 @@
+
+import java.awt.event.*;
+//package JenkinJava;
+public class Jenkins{
+
+public static void main(String args[]){
+ 
+  for(int i=1; i<=10; i++){
+  System.out.println("Hello World " + i);
+  
+  }
+   
+}
+}
